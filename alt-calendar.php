f7f2478bb1491e90262b2b87618a0baead4e5c34
KJastrzebski1 <krzysztof.jastrzebski@polcode.net>
2016-07-01 17:10:46 +0200

    Preparing before refactoring. Resolved conflicts and change minor things

--- alt-calendar.php
+++ alt-calendar.php
@@ -50,20 +50,23 @@ class Alt_Calendar {
 }
 
 function alt_enqueue_scripts() {
-
+    wp_deregister_script('jquery');
     wp_enqueue_style('fullCalendar_lib_css', plugins_url('fullcalendar/fullcalendar.min.css', __FILE__));
     wp_enqueue_style('event_panel', plugins_url('assets/css/event.css', __FILE__));
-    wp_enqueue_script('jQuery_lib', plugins_url('fullcalendar/lib/jquery.min.js', __FILE__));
-    wp_enqueue_script('momentjs', plugins_url('fullcalendar/lib/moment.min.js', __FILE__), ['jQuery_lib']);
+    wp_enqueue_style('awesomefonts', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
+    wp_enqueue_style('jquery_ui_css', plugins_url('fullcalendar/lib/cupertino/jquery-ui.min.css', __FILE__));
+    
+    wp_enqueue_script('jquery', plugins_url('fullcalendar/lib/jquery.min.js', __FILE__));
+    wp_enqueue_script('momentjs', plugins_url('fullcalendar/lib/moment.min.js', __FILE__), ['jquery']);
     wp_enqueue_script('momentjs_tz', 'http://momentjs.com/downloads/moment-timezone.min.js', ['momentjs']);
     wp_enqueue_script('fullCalendar_lib', plugins_url('fullcalendar/fullcalendar.min.js', __FILE__), ['momentjs']);
-    wp_enqueue_style('awesomefonts', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
-    wp_enqueue_script('jquery-ui', plugins_url('fullcalendar/lib/jquery-ui.min.js', __FILE__), ['jQuery_lib']);
-    wp_enqueue_style('jquery_ui_css', plugins_url('fullcalendar/lib/cupertino/jquery-ui.min.css', __FILE__));
+    
+    wp_enqueue_script('jquery-ui', plugins_url('fullcalendar/lib/jquery-ui.min.js', __FILE__), ['jquery'], false, true);
+    
     global $pagename;
 
     if ($pagename == 'alt-calendar') {
-        wp_enqueue_script('fullCalendar', plugins_url('assets/js/fullCalendar.js', __FILE__), ['fullCalendar_lib', 'jquery-ui']);
+        wp_enqueue_script('fullCalendar', plugins_url('assets/js/fullCalendar.js', __FILE__), ['fullCalendar_lib', 'jquery-ui'], false, true);
         wp_localize_script('fullCalendar', 'ajax_object', array(
             'ajax_url' => admin_url('admin-ajax.php'),
             'api_key' => 'AIzaSyBg5viJdIm0bBtQW6QVP1U7jx9OLevIUuw',
